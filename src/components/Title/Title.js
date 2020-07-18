6339daf824045e387fb6569d5959eb2a25f9d303
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2020-07-18 23:05:38 +0200

    change to function components & better syntax

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -2,13 +2,11 @@ import React from "react";
 import PropTypes from "prop-types";
 import styles from "./css/Title.module.css";
 
-const Title = (props) => {
-  return (
-    <header className={styles.Header}>
-      <h1 className={styles.Header__Title}>{props.name}</h1>
-    </header>
-  );
-};
+const Title = (props) => (
+  <header className={styles.Header}>
+    <h1 className={styles.Header__Title}>{props.name}</h1>
+  </header>
+);
 
 Title.propTypes = {
   name: PropTypes.string.isRequired,
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,16 +1,11 @@
-import React, { Component } from "react";
+import React from "react";
 import classes from "./css/Layout.module.css";
 
-class Layout extends Component {
-  state = {};
-  render() {
-    return (
-      <div className={classes.Layout}>
-        <main className={classes.Content}>{this.props.children}</main>
-        <footer className={classes.Footer}></footer>
-      </div>
-    );
-  }
-}
+const Layout = (props) => (
+  <div className={classes.Layout}>
+    <main className={classes.Content}>{props.children}</main>
+    <footer className={classes.Footer}></footer>
+  </div>
+);
 
 export default Layout;
