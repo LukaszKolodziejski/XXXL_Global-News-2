ba63692f2b2cd6446276b8164f91483e22883f41
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2020-07-19 20:29:38 +0200

    ClearFilters to fn() with React-Redux Hooks

--- src/components/Navigation/Filters/ClearFilters/ClearFilters.js
+++ src/components/Navigation/Filters/ClearFilters/ClearFilters.js
@@ -1,13 +1,19 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import PropTypes from "prop-types";
 import * as actions from "../../../../store/actions/index";
 
 import Button from "../../../UI/Button/Button";
 
 const ClearFilters = (props) => {
+  const filters = useSelector((state) => state.articlesIndex.filters);
+
+  const dispatch = useDispatch();
+
+  const onClearButtonHandler = (filters) =>
+    dispatch(actions.clearButtonHandler(filters));
+
   const clearButtonHandler = () => {
-    const { filters } = { ...this.props };
     filters.forEach((filter) => {
       filter.data.values.forEach((value) => (value.active = false));
       filter.data.dropdownName = filter.data.name;
@@ -15,8 +21,8 @@ const ClearFilters = (props) => {
       filter.api.active = false;
       filter.api.query = filter.api.startQuery;
     });
-    this.props.onClear();
-    this.props.onClearButtonHandler(filters);
+    props.onClear();
+    onClearButtonHandler(filters);
   };
   return (
     <Button btnType="Clearbtn" clicked={clearButtonHandler}>
@@ -25,42 +31,8 @@ const ClearFilters = (props) => {
   );
 };
 
-// class ClearFilters extends Component {
-//   state = {};
-
-//   clearButtonHandler = () => {
-//     const { filters } = { ...this.props };
-//     filters.forEach((filter) => {
-//       filter.data.values.forEach((value) => (value.active = false));
-//       filter.data.dropdownName = filter.data.name;
-//       filter.data.activeValue = "";
-//       filter.api.active = false;
-//       filter.api.query = filter.api.startQuery;
-//     });
-//     this.props.onClear();
-//     this.props.onClearButtonHandler(filters);
-//   };
-
-//   render() {
-//     return (
-//       <Button btnType="Clearbtn" clicked={this.clearButtonHandler}>
-//         Clear Filters
-//       </Button>
-//     );
-//   }
-// }
-
 ClearFilters.propTypes = {
   filters: PropTypes.array.isRequired,
 };
 
-const mapStateToProps = (state) => ({
-  filters: state.articlesIndex.filters,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  onClearButtonHandler: (filters) =>
-    dispatch(actions.clearButtonHandler(filters)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(ClearFilters);
+export default ClearFilters;
