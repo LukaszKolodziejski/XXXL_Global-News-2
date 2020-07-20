1c3b0087f708dbd87efbe4b35179c2ab73e36c8e
Łukasz Kołodziejski <lukasz-kolodziejski3333@wp.pl>
2020-07-20 08:49:11 +0200

    new component Information.js & Description.js

--- src/components/Articles/FullSingleArticle/FullSingleArticle.js
+++ src/components/Articles/FullSingleArticle/FullSingleArticle.js
@@ -3,17 +3,11 @@ import PropTypes from "prop-types";
 
 import styles from "./css/FullSingleArticle.module.css";
 import Button from "../../UI/Button/Button";
+import Information from "../../Information/Information";
+import Description from "../../Information/Description";
 
 const FullSingleArticle = React.memo((props) => {
   const { data } = props;
-  const convertDate = (date) => {
-    const d = new Date(date);
-    let newD = d.toString();
-    newD = `${newD.slice(4, 10)}, ${newD.slice(11, 15)}`;
-    return newD;
-  };
-  const convertPublishedAt = convertDate(data.publishedAt);
-
   return (
     <Fragment>
       <article className={styles.FullSingleArticle}>
@@ -21,16 +15,9 @@ const FullSingleArticle = React.memo((props) => {
           className={styles.FullSingleArticle__image}
           src={data.urlToImage}
         ></img>
-        <div className={styles.Information}>
-          <span className={styles.Information__data}>{convertPublishedAt}</span>
-          <span className={styles.Information__data}>{data.author}</span>
-          <a className={styles.Information__data} href={data.url}>
-            {data.source.name}
-          </a>
-          <h3 className={styles.FullSingleArticle__title}>{data.title}</h3>
-          <div className={styles.FullSingleArticle__description}>
-            {data.description}
-          </div>
+        <div className={styles.FullSingleArticle__information}>
+          <Information data={data} />
+          <Description data={data} />
           <a target="_blank" href={data.url}>
             <Button btnType="Source">Go to Source</Button>
           </a>
--- src/components/Articles/SingleArticle/SingleArticle.js
+++ src/components/Articles/SingleArticle/SingleArticle.js
@@ -4,17 +4,11 @@ import PropTypes from "prop-types";
 
 import styles from "./css/SingleArticle.module.css";
 import Button from "../../UI/Button/Button";
+import Information from "../../Information/Information";
+import Description from "../../Information/Description";
 
 const SingleArticle = React.memo((props) => {
-  const convertDate = (date) => {
-    const d = new Date(date);
-    let newD = d.toString();
-    newD = `${newD.slice(4, 10)}, ${newD.slice(11, 15)}`;
-    return newD;
-  };
-
   const { data, id } = props;
-  const convertPublishedAt = convertDate(data.publishedAt);
   return (
     <Fragment>
       <article className={styles.SingleArticle}>
@@ -22,21 +16,10 @@ const SingleArticle = React.memo((props) => {
           className={styles.SingleArticle__image}
           src={data.urlToImage}
         ></img>
-        <div className={styles.Information}>
-          <span className={styles.Information__data}>{convertPublishedAt}</span>
-          <span className={styles.Information__data}>{data.author}</span>
-          <a
-            className={styles.Information__data}
-            target="_blank"
-            href={data.url}
-          >
-            {data.source.name}
-          </a>
+        <div className={styles.SingleArticle__information}>
+          <Information data={data} target="_blank" />
         </div>
-        <h3 className={styles.SingleArticle__title}>{data.title}</h3>
-        <div className={styles.SingleArticle__description}>
-          {data.description}
-        </div>
+        <Description data={data} />
         <Link to="/article">
           <Button btnType="ReadMore" clicked={() => props.idHandler(id)}>
             Read More
